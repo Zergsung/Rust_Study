14a694440065466f4d0291a4b49ee0f305d6e96f
sungmo0317 <usungmo0317@gmail.com>
2023-06-28 18:02:39 +0900

    feat(fn main) : 계산 후 출력 구현
    연산 번호를 입력 받아서 각 번호에 맞게 연산 후 출력까지 구현했습니다.
    
    원본 코드는 자꾸 에러가 떴지만 gpt한테 수정 해달라고 한 후 에러가 안 뜨고 정상 작동 했습니다. 이유는 잘 모름.

--- simple_calculator/src/main.rs
+++ simple_calculator/src/main.rs
@@ -3,9 +3,9 @@ use std::io;
 fn main() {
     let mut get_num1 = String::new();
     let mut get_num2 = String::new();
-    
+
     io::stdin().read_line(&mut get_num1).expect("입력받지 못했습니다.");
-    let _get_num1: u32 = match get_num1.trim().parse() {
+    let get_num1: i32 = match get_num1.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             println!("숫자를 입력해주세요");
@@ -14,7 +14,7 @@ fn main() {
     };
 
     io::stdin().read_line(&mut get_num2).expect("입력받지 못했습니다.");
-    let _get_num1: u32 = match get_num2.trim().parse() {
+    let get_num2: i32 = match get_num2.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             println!("숫자를 입력해주세요");
@@ -23,17 +23,32 @@ fn main() {
     };
 
     print!("1. 더하기\n2. 빼기\n3. 곱하기\n4. 나누기\n");
-    
+
     let mut menu_num = String::new();
-    
+
     io::stdin().read_line(&mut menu_num).expect("입력받지 못했습니다.");
-    let _menu_num: u32 = match menu_num.trim().parse() {
+    let menu_num: u32 = match menu_num.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             println!("숫자를 입력해주세요");
             return;
         }
     };
-    
-    
+
+    match menu_num {
+        1 => println!("결과: {}", get_num1 + get_num2),
+        2 => println!("결과: {}", get_num1 - get_num2),
+        3 => println!("결과: {}", get_num1 * get_num2),
+        4 => {
+            if get_num2 == 0 {
+                println!("0으로 나눌 수 없습니다");
+            } {
+                let result = get_num1 as f64 / get_num2 as f64;
+                println!("결과: {}", result);
+            }
+        }
+        _ => {
+            println!("올바르지 않은 번호입니다.");
+        }
+    };
 }

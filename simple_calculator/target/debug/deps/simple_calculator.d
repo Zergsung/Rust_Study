32ef9da0084c9eeccce20005daf8407b20cc55dd
d0razi <usungmo0317@gmail.com>
2023-06-29 17:22:54 +0900

    Delete simple_calculator/target directory

--- simple_calculator/target/debug/deps/simple_calculator.d
+++
@@ -1,5 +0,0 @@
-E:\문서\d0razi's github\Rust_Study\simple_calculator\target\debug\deps\simple_calculator.exe: src\main.rs
-
-E:\문서\d0razi's github\Rust_Study\simple_calculator\target\debug\deps\simple_calculator.d: src\main.rs
-
-src\main.rs:
--- simple_calculator/target/debug/simple_calculator.d
+++
@@ -1,1 +0,0 @@
-E:\문서\d0razi's\ github\Rust_Study\simple_calculator\target\debug\simple_calculator.exe: E:\문서\d0razi's\ github\Rust_Study\simple_calculator\src\main.rs

7ba79edf3efb6087ebae09e0a786c930f9edc987
sungmo0317 <usungmo0317@gmail.com>
2023-06-29 17:03:11 +0900

    config

--- simple_calculator/target/debug/deps/simple_calculator-2177f0b5b3b012fa.d
+++ simple_calculator/target/debug/deps/simple_calculator-2177f0b5b3b012fa.d
@@ -1,5 +1,5 @@
-E:\문서\Rust_Study\simple_calculator\target\debug\deps\simple_calculator-2177f0b5b3b012fa.rmeta: src\main.rs
+E:\문서\d0razi's github\Rust_Study\simple_calculator\target\debug\deps\simple_calculator-2177f0b5b3b012fa.rmeta: src\main.rs
 
-E:\문서\Rust_Study\simple_calculator\target\debug\deps\simple_calculator-2177f0b5b3b012fa.d: src\main.rs
+E:\문서\d0razi's github\Rust_Study\simple_calculator\target\debug\deps\simple_calculator-2177f0b5b3b012fa.d: src\main.rs
 
 src\main.rs:
--- simple_calculator/target/debug/simple_calculator.d
+++ simple_calculator/target/debug/simple_calculator.d
@@ -1,1 +1,1 @@
-E:\문서\Rust_Study\simple_calculator\target\debug\simple_calculator.exe: E:\문서\Rust_Study\simple_calculator\src\main.rs
+E:\문서\d0razi's\ github\Rust_Study\simple_calculator\target\debug\simple_calculator.exe: E:\문서\d0razi's\ github\Rust_Study\simple_calculator\src\main.rs
